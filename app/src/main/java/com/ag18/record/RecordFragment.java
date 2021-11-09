da7f52d045447795fc1e7f4bb34206dd252e2732
HoangKhoi <thienkhoi0604@gmail.com>
2021-11-09 20:20:46 +0700

    lưu file nhưng chưa làm 2 button save delete

--- app/src/main/java/com/ag18/record/RecordFragment.java
+++ app/src/main/java/com/ag18/record/RecordFragment.java
@@ -1,11 +1,17 @@
 package com.ag18.record;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,6 +28,9 @@ public class RecordFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private NavController navController;
+    private ImageButton btnRecord;
 
     public RecordFragment() {
         // Required empty public constructor
@@ -58,6 +67,22 @@ public class RecordFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_record, container, false);
+        FrameLayout layout_record = (FrameLayout)inflater.inflate(R.layout.fragment_record, container, false);
+        return layout_record;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        navController = Navigation.findNavController(view);
+        btnRecord = (ImageButton)view.findViewById(R.id.button_topleft);
+
+        btnRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_voiceEditorFragment_to_recordingFragment);
+            }
+        });
     }
 }
